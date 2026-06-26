fa6d941126440644e124a16076365d7311c29340
jyothirganesh-0475 <23pa1a0475@vishnu.edu.in>
2026-06-26 11:05:56 +0530

    chore: update Google Client ID in frontend environments

--- EduExchange.Client/src/environments/environment.development.ts
+++ EduExchange.Client/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production:     true,
   apiUrl:         'https://eduexchange-api.onrender.com/api',
   apiBase:        'https://eduexchange-api.onrender.com',
-  googleClientId: '1076706795013-30eak61jjbpjdi5oml72vvof8j4d3s0c.apps.googleusercontent.com'
+  googleClientId: '1054515087406-5g93bl14ab0lmqa685uer6bhc2r96cin.apps.googleusercontent.com'
 };
